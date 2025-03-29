33a7d9c502e7887fb94dd7d84f2e089c19b4ddb8
sandhya <sandhya@LAPTOP-QBT2F4RV>
2025-03-29 15:20:05 +0530

    third commit

---
+++ src/test/java/githpackage/Login.java
@@ -0,0 +1,11 @@
+package githpackage;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	public void loginOperation() {
+		System.out.println("User logged in successfully");
+	}
+
+}
